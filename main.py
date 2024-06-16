c7aa9164487452753d6d1101cba5bc828a0f8eed
Emilio Nicoli <emilionicoli@gmail.com>
2024-06-15 22:00:04 -0600

    Update chat server port and refactor dependencies
    The chat server's port has been updated from 6677 to 8000. This required changes in several places including "index.html", "test.py", and "main.py". Additionally, dependencies have been refactored and moved around to reflect updates in the codebase structure.

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
 import asyncio
-from typing import AsyncIterable
+from typing import AsyncIterable, List
 import os
 import uvicorn
 from dotenv import load_dotenv
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, Header, Depends
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import StreamingResponse
 from langchain_community.chat_models import ChatOpenAI
-from langchain_core.messages import HumanMessage, SystemMessage
+from langchain_core.language_models import BaseChatModel
+from langchain_core.messages import HumanMessage, SystemMessage, AIMessage, BaseMessage
+from langchain_core.prompts import ChatPromptTemplate
+from typing import List, Optional
 from pydantic import BaseModel
 from langchain.callbacks.streaming_aiter import AsyncIteratorCallbackHandler
 from starlette.staticfiles import StaticFiles
+
+from utils.history import MessagesCollection
 
 load_dotenv()
 API_KEY = os.getenv('OPENAI_API_KEY', 'default_value_if_not_found')
@@ -36,7 +41,11 @@ def read_root():
 
 
 class Message(BaseModel):
+    role: str
     content: str
+
+class ChatHistory(BaseModel):
+    messages: List[Message]
 
 
 async def send_message(content: str) -> AsyncIterable[str]:
@@ -50,15 +59,14 @@ async def send_message(content: str) -> AsyncIterable[str]:
     )
 
     system_message = SystemMessage(content="""
-            Describe the following document with one of the following keywords:
-            Mateusz, Jakub, Adam. Return the keyword and nothing else.
+            You are a helpful assistant named Buddy.
         """)
 
     human_message = HumanMessage(content=content)
 
     task = asyncio.create_task(
-        model.agenerate(messages=[[HumanMessage(content=content)]])
-        #model.agenerate(messages=[[system_message, human_message]]))
+        # model.agenerate(messages=[[HumanMessage(content=content)]])
+        model.agenerate(messages=[[system_message, human_message]])
     )
 
     try:
@@ -75,6 +83,68 @@ async def send_message(content: str) -> AsyncIterable[str]:
 @app.post("/stream_chat/")
 async def stream_chat(message: Message):
     generator = send_message(message.content)
+    return StreamingResponse(generator, media_type="text/event-stream")
+
+
+# async def verify_authorization(authorization: Optional[str] = Header(None)):
+#     # if authorization != "Bearer my_key_something": # should be !=
+#     #     raise HTTPException(status_code=401, detail="Unauthorized")
+
+
+async def process_message_history(messages: List[Message]) -> AsyncIterable[str]:
+    callback = AsyncIteratorCallbackHandler()
+    model = ChatOpenAI(
+        streaming=True,
+        verbose=True,
+        callbacks=[callback],
+        openai_api_key=API_KEY,
+        model_name="gpt-3.5-turbo",
+    )
+    msgs: List[BaseMessage] = []
+    system_message = SystemMessage(content="""
+                You are a helpful assistant named Buddy.
+            """)
+
+    msgs.append(system_message)
+
+    for message in messages:
+        if message.role in ["human", "user"]:
+            msg = HumanMessage(content=message.content)
+        elif message.role in ["assistant", "ai"]:
+            msg = AIMessage(content=message.content)
+        else:
+            msg = HumanMessage(content=message.content)  # Default to HumanMessage
+        msgs.append(msg)
+
+    for message in messages:
+        if message.role in ["human", "user"]:
+            msg = HumanMessage(content=message.content)
+        else:
+            msg = HumanMessage(content=message.content)
+        msgs.append(msg)
+
+    task = asyncio.create_task(
+        model.agenerate(messages=[msgs])
+    )
+
+    try:
+        async for token in callback.aiter():
+            yield token
+    except Exception as e:
+        print(f"Caught exception: {e}")
+    finally:
+        callback.done.set()
+
+    await task
+
+
+@app.post("/stream_history/")
+async def stream_history(
+        chat_history: ChatHistory
+        # ,
+        # authorization: Optional[str] = Depends(verify_authorization)
+):
+    generator = process_message_history(chat_history.messages)
     return StreamingResponse(generator, media_type="text/event-stream")
 
 
