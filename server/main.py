8625da69e9c464ef15f9a4215e7ca566bfa7c03a
Emilio Nicoli <emilionicoli@gmail.com>
2024-06-16 23:45:29 -0600

    Refactor main.py and rename test files
    The main.py file was redesigned and moved to the server directory. Several changes including cleanup of unused imports and changes to variable naming were made. The OpenAI model name is now loaded from environment variables. Test files were also moved to the tests directory.

--- server/main.py
+++ server/main.py
@@ -7,9 +7,7 @@ from fastapi import FastAPI, HTTPException, Header, Depends
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import StreamingResponse
 from langchain_community.chat_models import ChatOpenAI
-from langchain_core.language_models import BaseChatModel
 from langchain_core.messages import HumanMessage, SystemMessage, AIMessage, BaseMessage
-from langchain_core.prompts import ChatPromptTemplate
 from typing import List, Optional
 from pydantic import BaseModel
 from langchain.callbacks.streaming_aiter import AsyncIteratorCallbackHandler
@@ -19,6 +17,7 @@ from utils.history import MessagesCollection
 
 load_dotenv()
 API_KEY = os.getenv('OPENAI_API_KEY', 'default_value_if_not_found')
+MODEL = os.getenv('MODEL', 'gpt-3.5-turbogpt-3.5-turbo')
 
 
 app = FastAPI()
@@ -98,14 +97,14 @@ async def process_message_history(messages: List[Message]) -> AsyncIterable[str]
         verbose=True,
         callbacks=[callback],
         openai_api_key=API_KEY,
-        model_name="gpt-3.5-turbo",
+        model_name=MODEL,
     )
-    msgs: List[BaseMessage] = []
+    new_message_list: List[BaseMessage] = []
     system_message = SystemMessage(content="""
                 You are a helpful assistant named Buddy.
             """)
 
-    msgs.append(system_message)
+    new_message_list.append(system_message)
 
     for message in messages:
         if message.role in ["human", "user"]:
@@ -114,10 +113,10 @@ async def process_message_history(messages: List[Message]) -> AsyncIterable[str]
             msg = AIMessage(content=message.content)
         else:
             msg = HumanMessage(content=message.content)  # Default to HumanMessage
-        msgs.append(msg)
+        new_message_list.append(msg)
 
     task = asyncio.create_task(
-        model.agenerate(messages=[msgs])
+        model.agenerate(messages=[new_message_list])
     )
 
     try:
@@ -134,8 +133,7 @@ async def process_message_history(messages: List[Message]) -> AsyncIterable[str]
 @app.post("/stream_history/")
 async def stream_history(
         chat_history: ChatHistory
-        # ,
-        # authorization: Optional[str] = Depends(verify_authorization)
+        # , authorization: Optional[str] = Depends(verify_authorization)
 ):
     generator = process_message_history(chat_history.messages)
     return StreamingResponse(generator, media_type="text/event-stream")
